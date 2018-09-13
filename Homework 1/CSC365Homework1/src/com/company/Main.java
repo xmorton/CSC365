9ec38b4337a9b35356aa045bb77f0dcd3c3251f6
Xavier <xmorton@oswego.edu>
2018-09-13 19:48:16 -0400

    Writing to a file

--- Homework 1/CSC365Homework1/src/com/company/Main.java
+++ Homework 1/CSC365Homework1/src/com/company/Main.java
@@ -1,8 +1,44 @@
 package com.company;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        String quit = new String();
+        Scanner scan = new Scanner(System.in);
+
+        try {
+            File fileName =  new File("C:\\Users\\xamor\\Documents\\GitHub\\CSC365\\Homework 1\\out\\production\\CSC365Homework1\\com\\company\\output.txt");
+            /*If file gets created then the createNewFile()
+             * method would return true or if the file is
+             * already present it would return false
+             */
+            boolean fvar = fileName.createNewFile();
+            if (fvar){
+                System.out.println("File has been created successfully");
+            }
+            else{
+                System.out.println("File already present at the specified location");
+            }
+
+            while (!quit.equals("quit")) {
+                System.out.println("Enter an input int");
+                String str = String.valueOf(scan.nextInt());
+                BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+                writer.append(' ');
+                writer.append(str);
+
+                writer.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Exception Occurred:");
+            e.printStackTrace();
+        }
+
     }
 }
